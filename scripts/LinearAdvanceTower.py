8737efc4143f4a383f93ea97ac87c224e6a89c04
Scott Ramsby <scramsby@microsoft.com>
2021-09-14 19:14:51 -0700

    Tweaks

--- scripts/LinearAdvanceTower.py
+++ scripts/LinearAdvanceTower.py
@@ -3,40 +3,52 @@
 # Created by Scott Ramsby
 
 import re
+from typing import Any, Dict, List, OrderedDict
 from ..Script import Script
 
 
 class LinearAdvanceTower(Script):
     _layer_regex = re.compile(r";LAYER:(\d+)")
 
-    def getSettingDataString(self):
-        return """{
+    def getSettingData(self) -> Dict[str, Any]:
+        return {
             "name": "Linear Advance Tower",
             "key": "LinearAdvanceTower",
             "metadata": {},
             "version": 2,
-            "settings":
-            {
-                "start":
-                {
-                    "label": "Starting K Factor",
-                    "description": "K factor for layer 0",
+            "settings": OrderedDict(
+                start={
+                    "label": "Starting Factor",
+                    "description": "Factor for layer 0",
                     "type": "float",
                     "default_value": 0
                 },
-                "increment":
-                {
-                    "label": "K Factor Increment",
-                    "description": "Increase of K factor per layer",
+                increment={
+                    "label": "Factor Increment",
+                    "description": "Increase of factor per layer",
                     "type": "float",
                     "default_value": 0.0005
+                },
+                offset={
+                    "label": "Offset",
+                    "description": "Number of layers to offset before layer 0",
+                    "type": "int",
+                    "default_value": 3
+                },
+                base={
+                    "label": "Base Factor",
+                    "description": "Factor for layers before layer 0",
+                    "type": "float",
+                    "default_value": 0.0415
                 }
-            }
-        }"""
+            )
+        }
 
-    def execute(self, data):
+    def execute(self, data: List[str]) -> List[str]:
         start = self.getSettingValueByKey("start")
         increment = self.getSettingValueByKey("increment")
+        offset = self.getSettingValueByKey("offset")
+        base = self.getSettingValueByKey("base")
 
         for index, layer in enumerate(data):
             lines = layer.split("\n")
@@ -44,7 +56,9 @@ class LinearAdvanceTower(Script):
                 match = LinearAdvanceTower._layer_regex.match(line)
                 if match:
                     layer_number = int(match.group(1))
+                    k_factor = base if layer_number < offset else (
+                        start + increment * (layer_number - offset))
                     data[index] = "\n".join(lines[:line_number] + [line] + [
-                                            f"M900 K{start + increment * layer_number}"] + lines[line_number + 1:])
+                                            f"M900 K{k_factor}"] + lines[line_number + 1:])
                     break
         return data
